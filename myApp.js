aebcc5034f9c3a401c18b32cea91d9067e2b7e69
Helder Tosta <helderatosta@gmail.com>
2023-01-02 17:32:11 -0300

    Criando primeira API retornando um simples JSON

--- myApp.js
+++ myApp.js
@@ -3,6 +3,10 @@ let app = express();
 
 app.use('/public', express.static(__dirname + '/public'));
 
+app.get('/json', (req, res) => {
+  res.json({"message": "Hello json"});
+});
+
 app.get('/', function(req, res) {
   //res.send("Hello Express");
   res.sendFile(__dirname + '/views/index.html');
