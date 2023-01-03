e1451977f45216f0a41cfa2a189c85e710538811
Helder Tosta <helderatosta@gmail.com>
2023-01-02 23:58:26 -0300

    Trabalhando com variáveis de ambiente com dotenv

--- myApp.js
+++ myApp.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/json', (req, res) => {
-  res.json({"message": "Hello json"});
+  if(process.env.MESSAGE_STYLE == 'uppercase'){
+    res.json({"message": "HELLO JSON"});
+  } else {
+    res.json({"message": "Hello json"});
+  }
 });
 
 app.get('/', function(req, res) {
